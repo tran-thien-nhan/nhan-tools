5f089da3e85104a7d714885081011a994da30bef
tran thien nhan <pipclupnomad@gmail.com>
2026-02-28 14:23:35 +0700

    ok

---
+++ app/home-workout-ai/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeWorkoutAI from '../_components/HomeWorkoutAI'
+
+const page = () => {
+    return (
+        <div>
+            <HomeWorkoutAI />
+        </div>
+    )
+}
+
+export default page
